902781242f9367afc1202b0c302c6e828a769632
Noor Taquee <noortaquee001@gmail.com>
2026-07-29 08:02:46 +0530

    feat: create TriadsElementCell component to display triads data

---
+++ src/components/traids-element-cell/index.tsx
@@ -0,0 +1,21 @@
+import type { TriadsElementData } from "../../pages/home-page/triads/data";
+
+interface ElementCellProps {
+  element: TriadsElementData;
+}
+
+export function TriadsElementCell({ element }: ElementCellProps) {
+  return (
+    <div className={`element-cell`}>
+      <div className="element-info-div">
+        <p className="mass-number">{Number(element.mass).toFixed(1)}</p>
+      </div>
+      <div className="element-name-div">
+        <p className="element-symbol">{element.symbol}</p>
+        <p className="element-name">{element.name}</p>
+      </div>
+    </div>
+  );
+}
+
+export default TriadsElementCell;
