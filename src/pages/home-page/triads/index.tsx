bc67f7be33d90f7aa174020858609f1d952bfcfd
Noor Taquee <noortaquee001@gmail.com>
2026-07-29 18:29:36 +0530

    feat: implement TriadsTable component to display triads data

---
+++ src/pages/home-page/triads/index.tsx
@@ -0,0 +1,37 @@
+import "./style.css";
+
+import { useEffect, useState } from "react";
+import { type TriadsData, loadElements } from "./data";
+import TriadsElementCell from "../../../components/traids-element-cell";
+
+export function TriadsTable() {
+  const [elements, setElements] = useState<TriadsData | null>(null);
+
+  useEffect(() => {
+    loadElements().then((data) => setElements(data));
+  }, []);
+
+  return (
+    <div className="element-table" id="triads-table">
+      {elements &&
+        elements.triads.map((group, groupNo) => (
+          <div
+            key={groupNo}
+            className={`triads-group ${group.name.toLowerCase().replaceAll(" ", "-")}`}
+          >
+            <p className="triads-group-title">{group.name}</p>
+            <div className="triads-group-elements">
+              {group.elements.map((element, elementNo) => (
+                <TriadsElementCell
+                  key={`triads-${groupNo}-${elementNo}`}
+                  element={element}
+                />
+              ))}
+            </div>
+          </div>
+        ))}
+    </div>
+  );
+}
+
+export default TriadsTable;
