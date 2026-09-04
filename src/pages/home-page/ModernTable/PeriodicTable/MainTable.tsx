ba5975725c7a6ce2798b3344cfa9593c31860c1f
Noor Taquee <noortaquee001@gmail.com>
2026-09-04 17:30:18 +0530

    update imports

--- src/pages/home-page/ModernTable/PeriodicTable/MainTable.tsx
+++ src/pages/home-page/ModernTable/PeriodicTable/MainTable.tsx
@@ -1,6 +1,6 @@
 import type { ElementData, TableData } from "../../../../core/data";
 
-import { ModernElementCell } from "../../../../components/ModernElementCell";
+import ModernElementCell from "../../../../components/ModernElementCell";
 
 interface MainTableProps {
   data: TableData;
